7d51b37288617b595c5839f67c297cbfc7658d61
Alex Sink <alexsink@alexsink.org>
2014-02-27 10:28:08 -0800

    After testing a few ways of getting some basic parameters passed around — i.e. auth for the apiStateManager, I realized what a pain I was going to cause myself if I kept on doing “one more thing manually wired up”, so started with a basic injection context and injection attribute to be able to wire things up without doing a ton of work.
    Will look at previous work and how to set the parameters (i.e. setting the IWebServiceRequest for calls without getting them to instantiate them)

---
+++ Assets/Transfluent/Editor/Network/Injections.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+namespace transfluent
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	public class Inject : Attribute
+	{
+		public string name;
+
+		public Inject()
+		{
+
+		}
+
+		public Inject(string injectionName)
+		{
+			name = injectionName;
+		}
+	}
+
+	public class UnboundInjectionException : Exception
+	{
+		public UnboundInjectionException() : base(){}
+		public UnboundInjectionException(string message) : base(message) { }
+	}
+
+	//right now only singleton mapping :(
+	public class InjectionContext
+	{
+		private Dictionary<Type, object> injectionMap = new Dictionary<Type, object>();
+		private Dictionary<string,Dictionary<Type,object>> namedInjectionMap = new Dictionary<string, Dictionary<Type, object>>(); 
+
+		public void addMapping(Type typeToHandle, object valueToPutIn)
+		{
+			injectionMap.Add(typeToHandle, valueToPutIn);
+		}
+
+		public void addMapping<T>(object valueToPutIn)
+		{
+			addMapping(typeof(T),valueToPutIn);
+		}
+
+		public void addNamedMapping<T>(string name, object valueToPutIn)
+		{
+			if(!namedInjectionMap.ContainsKey(name))
+				namedInjectionMap.Add(name,new Dictionary<Type, object>());
+			namedInjectionMap[name].Add(valueToPutIn.GetType(),valueToPutIn);
+		}
+
+		public void setMappings(object toInject)
+		{
+			setMappings(toInject, toInject.GetType());
+		}
+		public void setMappings(object toInject, Type type)
+		{
+			MemberInfo[] members = type.FindMembers(MemberTypes.Property, BindingFlags.Instance | BindingFlags.FlattenHierarchy | BindingFlags.SetProperty | BindingFlags.Public, null, null);
+
+			foreach(MemberInfo member in members)
+			{
+				object[] injections = member.GetCustomAttributes(typeof(Inject), true);
+				if(injections.Length > 0)
+				{
+					var propertyInfo = member as PropertyInfo;
+					var injectionAttribute = injections[0] as Inject; //TOOD: handle named stuff?
+					Type typeToInject = propertyInfo.PropertyType;
+					try
+					{
+						var injectionMapToUse = injectionMap;
+						if (!string.IsNullOrEmpty(injectionAttribute.name))
+							injectionMapToUse = namedInjectionMap[injectionAttribute.name];
+
+						object valueToInject = injectionMapToUse[typeToInject];
+						propertyInfo.SetValue(toInject, valueToInject, null);
+					}
+					catch (KeyNotFoundException k)
+					{
+						throw new UnboundInjectionException("Injeciton not set for type:" + typeToInject.Name + " when trying to set on a sub object");
+					}
+					
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/TransfluentTests/Editor/InjectionTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using transfluent;
+
+[TestFixture]
+public class InjectionTests 
+{
+	public class TestInjectionTarget
+	{
+		[Inject]
+		public string testString { get; set; }
+		[Inject("Special")]
+		public string specialString { get; set; }
+	}
+
+	[TestFixtureSetUp]
+	public void setupTheTests()
+	{
+		
+	}
+
+	[Test]
+	public void CreateContextSetString()
+	{
+		InjectionContext context = new InjectionContext();
+		string testText = "HELLO WORLD";
+		context.addMapping<string>(testText);
+		var target = new TestInjectionTarget();
+		Assert.IsNullOrEmpty(target.testString);
+		context.setMappings(target);
+		Assert.IsNotNullOrEmpty(target.testString);
+		Assert.AreEqual(target.testString,testText);
+	}
+
+	[Test]
+	[ExpectedException(typeof(UnboundInjectionException))]
+	public void CreateContextAndFail()
+	{
+		InjectionContext context = new InjectionContext();
+		//context.addMapping<string>("HELLO WORLD");
+		var target = new TestInjectionTarget();
+		Assert.IsNullOrEmpty(target.testString);
+		context.setMappings(target);
+		Assert.IsNotNullOrEmpty(target.testString);
+	}
+
+	[Test]
+	public void TestNamedInjection()
+	{
+		InjectionContext context = new InjectionContext();
+		string standardKey = "HELLO WORLD";
+		string namedKey = "SPECIAL STRING";
+		context.addMapping<string>(standardKey);
+		context.addNamedMapping<string>("Special", namedKey);
+		var target = new TestInjectionTarget();
+		Assert.IsNullOrEmpty(target.testString);
+		context.setMappings(target);
+		Assert.AreSame(target.testString,standardKey);
+		Assert.AreSame(target.specialString, namedKey);
+		Assert.AreNotEqual(standardKey,namedKey);
+		Assert.AreNotEqual(target.testString, target.specialString);
+	}
+}

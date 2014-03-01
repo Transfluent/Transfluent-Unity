abb4a50f46e9f27168e7402a1189e932759b7727
Alex Sink <alexsink@alexsink.org>
2014-02-28 16:55:28 -0800

    simple ongui text displayer

---
+++ Assets/Transfluent/InternationalTextDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class InternationalTextDisplay : MonoBehaviour
+{
+	[SerializeField] string textToDisplay = "我是一个中国人的一句。";
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnGUI()
+	{
+		GUILayout.TextField(textToDisplay);
+	}
+}

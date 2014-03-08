fe8d40eb8adb611a7dfd47f75c55f54ec5b195a4
Alex Sink <alexsink@alexsink.org>
2014-03-08 11:35:38 -0800

    hello does not require an auth token

--- Assets/Transfluent/Plugins/Calls/Hello.cs
+++ Assets/Transfluent/Plugins/Calls/Hello.cs
@@ -6,8 +6,6 @@ namespace transfluent
 	[Route("hello", RestRequestType.GET, "http://transfluent.com/backend-api/#Hello")]
 	public class Hello : WebServiceParameters
 	{
-		[Inject(NamedInjections.API_TOKEN)]
-		public string authToken { get; set; }
 
 		public Hello(string name)
 		{

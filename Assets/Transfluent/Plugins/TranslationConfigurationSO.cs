a2f1c378d9c58b2027568712e65bb1049d2b4199
Alex Sink <alexsink@alexsink.org>
2014-03-24 17:06:22 -0700

    don’t give the impression that you can change translation set group

--- Assets/Transfluent/Plugins/TranslationConfigurationSO.cs
+++ Assets/Transfluent/Plugins/TranslationConfigurationSO.cs
@@ -8,7 +8,7 @@ public class TranslationConfigurationSO : ScriptableObject
 	public TransfluentLanguage sourceLanguage;
 
 	//allows for multiple clients to run simultaniously, so you can have 2 names in 2 different namespaces -- translates to "group id" for transfluent
-	public string translation_set_group;
+	[HideInInspector] public string translation_set_group;
 
 	//so that sets of translations to clash, like a namespace.  means group_id for the transfluent api, but wou
 }

577ed3210893f557449aa4364c0256254170f16b
Alex Sink <alexsink@alexsink.org>
2014-04-17 14:54:29 -0700

    Fixes and improvements from strange rocks Import mode —scans for any unknown strings and saves them in the game translation set  (NOTE: not sure if it respects game sets right now…)
    FIX: GUILayout and GUI did not translate in some instances — note: functions not supported due to being text arrays (and I don’t want to mess with array creation/manipulation):
    Toolbar - array of texts is too much — do it yourself
    SelectionGrid

--- Assets/Transfluent/Runtime/TranslationUtility.cs
+++ Assets/Transfluent/Runtime/TranslationUtility.cs
@@ -11,7 +11,7 @@ namespace transfluent
 	{
 		//TODO: keep sets of language/group and allow for explict load/unload statements
 		//the implication of that is that any ongui/other client would need to declare set groups for their activiites in some way
-		private static readonly TransfluentUtilityInstance _instance = createNewInstance();
+		private static ITransfluentUtilityInstance _instance = createNewInstance();
 
 		private static LanguageList _LanguageList;
 
@@ -20,7 +20,7 @@ namespace transfluent
 			changeStaticInstanceConfigBasedOnTranslationConfigurationGroup(); //load default translation group info
 		}
 
-		public static TransfluentUtilityInstance getUtilityInstanceForDebugging()
+		public static ITransfluentUtilityInstance getUtilityInstanceForDebugging()
 		{
 			changeStaticInstanceConfigBasedOnTranslationConfigurationGroup();
 			return _instance;
@@ -39,7 +39,7 @@ namespace transfluent
 		{
 			//Debug.LogError("LOADING STATIC CONFIG: "+ destinationLanguageCode + " translation group:"+translationGroup);
 
-			TransfluentUtilityInstance tmpInstance = createNewInstance(destinationLanguageCode, translationGroup);
+			ITransfluentUtilityInstance tmpInstance = createNewInstance(destinationLanguageCode, translationGroup);
 			if(tmpInstance != null)
 			{
 				_instance.setNewDestinationLanguage(tmpInstance.allKnownTranslations);
@@ -68,7 +68,7 @@ namespace transfluent
 		public static void testHelpfind()
 		{
 			//GetComponentsInChildren( typeof(Transform), true );
-			var gos = GameObject.FindSceneObjectsOfType(typeof (GameObject)) as GameObject[];
+			var gos = GameObject.FindObjectsOfType<GameObject>();
 			foreach (GameObject go in gos)
 			{
 				if (go.name == "idle_view")
@@ -92,7 +92,7 @@ namespace transfluent
 		}
 		//public static event Action OnLanguageChanged;
 		
-		public static TransfluentUtilityInstance createNewInstance(string destinationLanguageCode = "", string group = "")
+		public static ITransfluentUtilityInstance createNewInstance(string destinationLanguageCode = "", string group = "")
 		{
 			if(_LanguageList == null)
 			{
@@ -104,16 +104,27 @@ namespace transfluent
 				Debug.LogError("Could not load new language list");
 				return null;
 			}
+			bool enableCapture = false;
+#if UNITY_EDITOR
+			if (Application.isEditor)
+			{
+				enableCapture = getCaptureMode();
+			}
+#endif //UNTIY_EDITOR
 
 			TransfluentLanguage dest = _LanguageList.getLangaugeByCode(destinationLanguageCode);
 			if (dest == null)
 			{
 				TranslationConfigurationSO defaultConfigInfo = ResourceLoadFacade.LoadConfigGroup(group);
 				string newDestinationLanguageCode = defaultConfigInfo.sourceLanguage.code;
-				if(string.IsNullOrEmpty(destinationLanguageCode))
+				/*
+				if (string.IsNullOrEmpty(destinationLanguageCode))
+				{
 					Debug.Log("Using default destination language code, as was given an empty language code");
+				}
 				else
-				Debug.Log("Could not load destination language code:" + destinationLanguageCode + " so falling back to source game language code:" + destinationLanguageCode);
+					Debug.Log("Could not load destination language code:" + destinationLanguageCode + " so falling back to source game language code:" + destinationLanguageCode);
+				 */
 				destinationLanguageCode = newDestinationLanguageCode;
 				
 				dest = _LanguageList.getLangaugeByCode(destinationLanguageCode);
@@ -126,12 +137,25 @@ namespace transfluent
 #if UNITY_EDITOR
 			EditorUtility.SetDirty(destLangDB);
 #endif
-			return new TransfluentUtilityInstance
+			
+			var newTranslfuentUtilityInstance = new TransfluentUtilityInstance
 			{
 				allKnownTranslations = keysInLanguageForGroupSpecified,
 				destinationLanguage = dest,
-				groupBeingShown = group
+				groupBeingShown = group,
 			};
+			if (enableCapture)
+			{
+				newTranslfuentUtilityInstance = new AutoCaptureTranslationUtiliityInstance()
+				{
+					allKnownTranslations = keysInLanguageForGroupSpecified,
+					destinationLanguage = dest,
+					groupBeingShown = group,
+					doCapture = enableCapture,
+					coreTransltionSet = destLangDB,
+				};
+			}
+			return newTranslfuentUtilityInstance;
 		}
 
 		public static string get(string sourceText)
@@ -145,12 +169,42 @@ namespace transfluent
 		{
 			return _instance.getFormattedTranslation(sourceText, formatStrings);
 		}
+		[MenuItem("Helpers/Enable Capture Mode")]
+		static void EnableCaptureMode()
+		{
+			setCaptureMode(true);
+			_instance = createNewInstance();
+		}
+
+		[MenuItem("Helpers/Disable Capture Mode")]
+		static void DisableCaptureMode()
+		{
+			setCaptureMode(false);
+			_instance = createNewInstance();
+		}
+
+		static bool getCaptureMode()
+		{
+			return EditorPrefs.GetBool("CAPTURE_MODE");
+		}
+		static void setCaptureMode(bool toCapture)
+		{
+			EditorPrefs.SetBool("CAPTURE_MODE",true);
+		}
 	}
 
 	//an interface for handling translaitons
-	public class TransfluentUtilityInstance
+	public interface ITransfluentUtilityInstance
 	{
-		public Dictionary<string, string> allKnownTranslations;
+		void setNewDestinationLanguage(Dictionary<string, string> transaltionsInSet);
+		string getFormattedTranslation(string sourceText, params object[] formatStrings);
+		string getTranslation(string sourceText);
+		Dictionary<string, string> allKnownTranslations { get; set; }
+	}
+
+	public class TransfluentUtilityInstance : ITransfluentUtilityInstance
+	{
+		public Dictionary<string, string> allKnownTranslations { get; set; }
 
 		public TransfluentLanguage destinationLanguage { get; set; }
 
@@ -172,11 +226,63 @@ namespace transfluent
 
 		public string getTranslation(string sourceText)
 		{
-			if(allKnownTranslations != null && allKnownTranslations.ContainsKey(sourceText))
+			if (allKnownTranslations != null)
 			{
-				return allKnownTranslations[sourceText];
+				if (allKnownTranslations.ContainsKey(sourceText))
+				{
+					return allKnownTranslations[sourceText];
+				}
 			}
+			else
+			{
+				//Debug.LogError("KNOWN TRANSLATIONS NOT SET");
+			}
+			
 			return sourceText;
+		}
+	}
+
+	public class AutoCaptureTranslationUtiliityInstance : TransfluentUtilityInstance, ITransfluentUtilityInstance
+	{
+		public bool doCapture { get; set; }
+		public GameTranslationSet coreTransltionSet { get; set; }
+
+		List<string> formattedTextToIgnore = new List<string>(); 
+
+		public new void setNewDestinationLanguage(Dictionary<string, string> transaltionsInSet)
+		{
+			Debug.LogWarning("SWITCHING Language, likely not intentional for auto capture transltion utility");
+			coreTransltionSet = null;
+		}
+
+		public new string getFormattedTranslation(string sourceText, params object[] formatStrings)
+		{
+			string formattedString = string.Format(getTranslation(sourceText), formatStrings);
+			if (!formattedTextToIgnore.Contains(formattedString))
+			{
+				formattedTextToIgnore.Add(formattedString);
+			}
+			return formattedString;
+		}
+
+		public new string getTranslation(string sourceText)
+		{
+			var translation = base.getTranslation(sourceText);
+			
+			if(allKnownTranslations != null)
+			{
+				if(doCapture && !allKnownTranslations.ContainsKey(sourceText) && !formattedTextToIgnore.Contains(sourceText))
+				{
+					allKnownTranslations.Add(sourceText, sourceText);
+					//TODO: find a cleaner way to add a better way to manually add a translation here 
+					coreTransltionSet.mergeInSet(groupBeingShown,allKnownTranslations);
+				}
+				if (formattedTextToIgnore.Contains(sourceText))
+				{
+					//Debug.LogError("blacklisted formatted translation:" + sourceText);
+				}
+			}
+			return translation;
 		}
 	}
 }

8bf698b33246cb2dfbe44ccb07a1e72482e2090a
Alex Sink <alexsink@alexsink.org>
2014-04-17 10:45:29 -0700

    Merging in changes from strange rocks test  ( currently at https://github.com/hardcoded2/strangerocks .. will likely get ported over to main account) OnLanguageChange() static helper in TranslationUtility will let all game objects in the scene know that
    changed name to TranslationUtility

--- Assets/Transfluent/Runtime/TranslationUtility.cs
+++ Assets/Transfluent/Runtime/TranslationUtility.cs
@@ -1,38 +1,88 @@
 ﻿using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEditor;
+#if UNITY_EDITOR
 using UnityEngine;
+#endif
 
 namespace transfluent
 {
-	public class TransfluentUtility
+	public class TranslationUtility
 	{
 		//TODO: keep sets of language/group and allow for explict load/unload statements
 		//the implication of that is that any ongui/other client would need to declare set groups for their activiites in some way
-		private static TransfluentUtilityInstance _instance = new TransfluentUtilityInstance();
+		private static readonly TransfluentUtilityInstance _instance = createNewInstance();
 
 		private static LanguageList _LanguageList;
 
-		private TransfluentUtility()
+		private TranslationUtility()
 		{
-			changeStaticInstanceConfig(); //load default translation group info
+			changeStaticInstanceConfigBasedOnTranslationConfigurationGroup(); //load default translation group info
 		}
 
 		public static TransfluentUtilityInstance getUtilityInstanceForDebugging()
 		{
+			changeStaticInstanceConfigBasedOnTranslationConfigurationGroup();
 			return _instance;
+		}
+
+		public static void changeStaticInstanceConfigBasedOnTranslationConfigurationGroup(string group="")
+		{
+			var config = ResourceLoadFacade.LoadConfigGroup(group);
+			if(config == null) Debug.LogWarning("No default translation configuration found");
+
+			changeStaticInstanceConfig(config.sourceLanguage.code, group);
 		}
 
 		//convert into a factory?
 		public static bool changeStaticInstanceConfig(string destinationLanguageCode = "", string translationGroup = "")
 		{
+			//Debug.LogError("LOADING STATIC CONFIG: "+ destinationLanguageCode + " translation group:"+translationGroup);
+
 			TransfluentUtilityInstance tmpInstance = createNewInstance(destinationLanguageCode, translationGroup);
 			if(tmpInstance != null)
 			{
-				_instance = tmpInstance;
+				_instance.setNewDestinationLanguage(tmpInstance.allKnownTranslations);
+				if(Application.isPlaying)
+					OnLanguageChanged();
+
 				return true;
 			}
+			
 			return false;
 		}
 
+		[MenuItem("Helpers/Test Change to EN-US")]
+		public static void ChangeStaticConfigToUS()
+		{
+			changeStaticInstanceConfig("en-us");
+		}
+
+		[MenuItem("Helpers/Test Change to FR-FR")]
+		public static void ChangeStaticConfigToFRFR()
+		{
+			changeStaticInstanceConfig("fr-fr");
+		}
+
+		[MenuItem("Helpers/Test find")]
+		public static void testHelpfind()
+		{
+			//GetComponentsInChildren( typeof(Transform), true );
+		}
+
+		[MenuItem("Helpers/Test OnLocalize")]
+		public static void OnLanguageChanged()
+		{
+			GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+			for (int i=0;i<allObjects.Length;i++)
+			{
+				GameObject go = allObjects[i];
+				if (go.transform.parent != null) continue; //skip any non-root messages
+				go.BroadcastMessage("OnLocalize",options:SendMessageOptions.DontRequireReceiver);
+			}
+		}
+		//public static event Action OnLanguageChanged;
+		
 		public static TransfluentUtilityInstance createNewInstance(string destinationLanguageCode = "", string group = "")
 		{
 			if(_LanguageList == null)
@@ -47,10 +97,26 @@ namespace transfluent
 			}
 
 			TransfluentLanguage dest = _LanguageList.getLangaugeByCode(destinationLanguageCode);
+			if (dest == null)
+			{
+				TranslationConfigurationSO defaultConfigInfo = ResourceLoadFacade.LoadConfigGroup(group);
+				string newDestinationLanguageCode = defaultConfigInfo.sourceLanguage.code;
+				if(string.IsNullOrEmpty(destinationLanguageCode))
+					Debug.Log("Using default destination language code, as was given an empty language code");
+				else
+				Debug.Log("Could not load destination language code:" + destinationLanguageCode + " so falling back to source game language code:" + destinationLanguageCode);
+				destinationLanguageCode = newDestinationLanguageCode;
+				
+				dest = _LanguageList.getLangaugeByCode(destinationLanguageCode);
+				//dest = _LanguageList.getLangaugeByCode
+			}
 			GameTranslationSet destLangDB = GameTranslationGetter.GetTranslaitonSetFromLanguageCode(destinationLanguageCode);
 			Dictionary<string, string> keysInLanguageForGroupSpecified = destLangDB != null
 				? destLangDB.getGroup(group).getDictionaryCopy()
 				: new Dictionary<string, string>();
+#if UNITY_EDITOR
+			EditorUtility.SetDirty(destLangDB);
+#endif
 			return new TransfluentUtilityInstance
 			{
 				allKnownTranslations = keysInLanguageForGroupSpecified,
@@ -83,7 +149,8 @@ namespace transfluent
 
 		public void setNewDestinationLanguage(Dictionary<string, string> transaltionsInSet)
 		{
-			allKnownTranslations.Clear();
+			if(allKnownTranslations != null)
+				allKnownTranslations.Clear();
 			allKnownTranslations = transaltionsInSet;
 		}
 

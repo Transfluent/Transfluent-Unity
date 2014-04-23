75a11001071951ef6bc34e699efbc8ae12d5ca42
Alex Sink <alexsink@alexsink.org>
2014-04-23 14:37:31 -0700

    a new sample with a simple ongui and a text mesh to boot

---
+++ Assets/Transfluent/Examples/Example_NegativeOne_OnGUI.cs
@@ -0,0 +1,25 @@
+﻿using transfluent;
+using UnityEngine;
+using GUILayout = transfluent.guiwrapper.GUILayout;
+
+public class Example_NegativeOne_OnGUI : MonoBehaviour
+{
+	private bool languageSelectToggle;
+
+	public void OnGUI()
+	{
+		GUILayout.Label("options");
+		languageSelectToggle = GUILayout.Toggle(languageSelectToggle, "Language Select");
+		if(languageSelectToggle)
+		{
+			if(GUILayout.Button("English"))
+				TranslationUtility.changeStaticInstanceConfig("en-us");
+			if(GUILayout.Button("French"))
+				TranslationUtility.changeStaticInstanceConfig("fr-fr");
+			if(GUILayout.Button("German"))
+				TranslationUtility.changeStaticInstanceConfig("de-de");
+			if(GUILayout.Button("Backwards (test language)"))
+				TranslationUtility.changeStaticInstanceConfig("xx-xx");
+		}
+	}
+}

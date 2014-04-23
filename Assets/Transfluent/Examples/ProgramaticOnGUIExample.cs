6e84034bb6c86ba2f1d0b6f2aef1b13f37f64d90
Alex Sink <alexsink@alexsink.org>
2014-04-23 15:47:07 -0700

    Two more examples.  I’m not sure they are pointed enough, but it’s something One showing off two different ways of interacting with our api (and switching languages) the other showing the utilities that are provided to make it a little easier

---
+++ Assets/Transfluent/Examples/ProgramaticOnGUIExample.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using transfluent;
+
+public class ProgramaticOnGUIExample : MonoBehaviour
+{
+	public LocalizeUtil managedText;
+	public string programaticallyManagedTextKey;
+	public string programaticallyManagedText;
+	private bool languageSelectToggle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnGUI()
+	{
+		//2 different ways of programmatically getting text
+		GUILayout.Label(managedText.current);
+		GUILayout.Label(programaticallyManagedText);
+		languageSelectToggle = GUILayout.Toggle(languageSelectToggle, "Language Select");
+
+		if(languageSelectToggle)
+		{
+			if(GUILayout.Button("English"))
+				TranslationUtility.changeStaticInstanceConfig("en-us");
+			if(GUILayout.Button("French"))
+				TranslationUtility.changeStaticInstanceConfig("fr-fr");
+			if(GUILayout.Button("German"))
+				TranslationUtility.changeStaticInstanceConfig("de-de");
+			if(GUILayout.Button("Backwards (test language)"))
+				TranslationUtility.changeStaticInstanceConfig("xx-xx");
+		}
+	}
+
+	public void OnEnable()
+	{
+		OnLocalize();
+	}
+	public void OnLocalize()
+	{
+		managedText.OnLocalize();
+		programaticallyManagedText = TranslationUtility.get(programaticallyManagedTextKey);
+	}
+}
---
+++ Assets/Transfluent/Examples/ProgrammaticUsingGUIWrapper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using GUILayout = transfluent.guiwrapper.GUILayout; //use wrappers to automatically translate your text
+
+public class ProgramaticUsingGUIWrapper : MonoBehaviour
+{
+	public string keyTransformedByWrappers;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(keyTransformedByWrappers);
+		GUILayout.Label("Begin!");
+	}
+}

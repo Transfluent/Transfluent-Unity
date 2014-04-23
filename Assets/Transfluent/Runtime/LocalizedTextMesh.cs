a99dad6ca5f40e47a4c7732ee98a93de45becef0
Alex Sink <alexsink@alexsink.org>
2014-04-23 12:45:33 -0700

    split out localized state util from localized text mesh.  other custom scripts probably want to use it.  it deserves it’s own file

---
+++ Assets/Transfluent/Runtime/LocalizedTextMesh.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using transfluent;
+using UnityEngine;
+
+public class LocalizedTextMesh : MonoBehaviour
+{
+	public bool textIsManagedExternally;  //if someone else is managing this
+
+	public TextMesh textmesh; //gets set in editor
+
+	public LocalizeUtil localizableText = new LocalizeUtil();
+
+	public void OnLocalize()
+	{
+		if(textIsManagedExternally) return;
+		localizableText.OnLocalize();
+		textmesh.text = localizableText.current;
+	}
+
+	public void OnEnable()
+	{
+		OnLocalize();
+	}
+#if UNITY_EDITOR
+	public void OnValidate()
+	{
+		textmesh.text = localizableText.current;  //make sure to update the textmesh
+	}
+#endif
+
+	public void Start()
+	{
+		textmesh.text = localizableText.current;
+	}
+}
+
+//Simple interface for providing the current text and interacting with the localization api
+[Serializable]
+public class LocalizeUtil
+{
+	public string globalizationKey;
+	//[SerializeField]
+	private string _current;
+
+	public string current
+	{
+		get
+		{
+			if(string.IsNullOrEmpty(globalizationKey))
+				return "";
+			return _current ?? (_current = TranslationUtility.get(globalizationKey));
+		}
+		//set { globalizationKey = _current; }
+	}
+
+	public string OnLocalize()
+	{
+		_current = null;
+		return current;
+	}
+
+	public static implicit operator string(LocalizeUtil util)
+	{
+		return util.current;
+	}
+
+	public override string ToString()
+	{
+		return current;
+	}
+}

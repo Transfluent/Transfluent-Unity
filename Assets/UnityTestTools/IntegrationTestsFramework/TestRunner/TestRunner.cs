3e3d20d084fe7b6fc0ac6496cdeb0314b370147d
Alex Sink <alexsink@alexsink.org>
2014-04-28 13:48:51 -0700

    another tweak to test runner

--- Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/TestRunner.cs
+++ Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/TestRunner.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityTest.IntegrationTestRunner;
@@ -235,7 +234,12 @@ namespace UnityTest
 				if(Application.loadedLevel < Application.levelCount - 1)
 					Application.LoadLevel(Application.loadedLevel + 1);
 				else
-					EditorApplication.Exit(0);
+				{
+#if UNITY_EDITOR
+					UnityEditor.EditorApplication.Exit(0);
+#endif
+				}
+					
 			}
 #else
 			if(Application.loadedLevel < Application.levelCount - 1)
